1d045e2ed84b3c259b7c843fc9a4a8481c979887
rmtsukuru <samcollegeemail@gmail.com>
2018-03-07 00:43:14 -0500

    Started building out a combat menu UI.

--- js/scenario.js
+++ js/scenario.js
@@ -12,6 +12,33 @@ Scene.prototype.draw = function() {
     drawText('DISPLAY DUMMY TEXT', BASE_HEIGHT * ASPECT_RATIO / 2 - 80, BASE_HEIGHT / 2 - 6);
 };
 
+function CombatScene() {
+    Scene.call(this);
+    this.menuY = 0;
+}
+
+CombatScene.prototype = Object.create(Scene.prototype);
+
+CombatScene.prototype.update = function() {
+    if (triggerKeyState.down) {
+        this.menuY++;
+        this.menuY %= 3;
+    }
+    else if (triggerKeyState.up) {
+        this.menuY--;
+        this.menuY %= 3;
+    }
+    updateInput();
+};
+
+CombatScene.prototype.draw = function() {
+    Scene.prototype.draw.call(this);
+    drawText('Attack', 50, 100);
+    drawText('Tactics', 50, 120);
+    drawText('Magic', 50, 140);
+    drawRect(30, 88 + 20 * this.menuY, 10, 10, 'white');
+}
+
 function configureScenario() {
-    scene = new Scene();
+    scene = new CombatScene();
 }
