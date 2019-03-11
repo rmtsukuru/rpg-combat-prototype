54532d336bbaf1ee53d824e701370aeddda8e1db
rmtsukuru <samcollegeemail@gmail.com>
2019-03-11 18:17:34 -0400

    Refactored to use more arrow functions.

--- js/condition.js
+++ js/condition.js
@@ -4,10 +4,10 @@ function Condition(target, stats, options) {
     this.resistance = options.resistance ? target.resistances[options.resistance] : null;
     this.duration = stats.duration || 1;
     this.time = this.duration;
-    this.onStart = stats.onStart || function() {};
-    this.onEnd = stats.onEnd || function() {};
-    this.onTimeTick = stats.onTimeTick || function() {};
-    this.onTurnTick = stats.onTurnTick || function() {};
+    this.onStart = stats.onStart || (() => {});
+    this.onEnd = stats.onEnd || (() => {});
+    this.onTimeTick = stats.onTimeTick || (() => {});
+    this.onTurnTick = stats.onTurnTick || (() => {});
     this.stackMax = stats.stackMax || 1;
 }
 
@@ -39,14 +39,14 @@ Condition.prototype.turnTick = function() {
 }
 
 const conditionData = {
-    dodge: { duration: 1, onStart: function(x) { x.evasion += 0.3; }, onEnd: function(x) { x.evasion -= 0.3; } },
-    silver: { text: 'The target\'s defense was lowered.', duration: 2, onStart: function(x) { x.defenses.laceration -= 8; x.defenses.penetration -= 8; }, onEnd: function(x) { x.defenses.laceration += 8; x.defenses.penetration += 8; } },
-    aim: { duration: 3, onStart: function(x) { x.critChance += 0.3; }, onEnd: function(x) { x.critChance -= 0.3; } },
-    prone: { duration: 3, onStart: function(x) { x.evasion -= 0.3; }, onEnd: function(x) { x.evasion += 0.3; } },
-    pestilence: { duration: 4, onStart: function(x) { x.hitChance -= 0.3; x.evasion -= 0.3 }, onEnd: function(x) { x.hitChance += 0.3; x.evasion += 0.3; } },
-    bleed: { text: 'The target is bleeding.', duration: 5, onTurnTick: function(x) { x.health -= 5; } },
-    poison: { text: 'The target was poisoned.', duration: 2, onTimeTick: function(x) { x.health -= 1; } },
-    jaegerbrau: { duration: 5, onStart: function(x) { x.evasion += 0.2; x.hitChance += 0.1; x.critChance += 0.2; }, onEnd: function(x) { x.evasion -= 0.2; x.hitChance -= 0.1; x.critChance -= 0.2; } },
+    dodge: { duration: 1, onStart: x => { x.evasion += 0.3; }, onEnd: x => { x.evasion -= 0.3; } },
+    silver: { text: 'The target\'s defense was lowered.', duration: 2, onStart: x => { x.defenses.laceration -= 8; x.defenses.penetration -= 8; }, onEnd: x => { x.defenses.laceration += 8; x.defenses.penetration += 8; } },
+    aim: { duration: 3, onStart: x => { x.critChance += 0.3; }, onEnd: x => { x.critChance -= 0.3; } },
+    prone: { duration: 3, onStart: x => { x.evasion -= 0.3; }, onEnd: x => { x.evasion += 0.3; } },
+    pestilence: { duration: 4, onStart: x => { x.hitChance -= 0.3; x.evasion -= 0.3 }, onEnd: x => { x.hitChance += 0.3; x.evasion += 0.3; } },
+    bleed: { text: 'The target is bleeding.', duration: 5, onTurnTick: x => { x.health -= 5; } },
+    poison: { text: 'The target was poisoned.', duration: 2, onTimeTick: x => { x.health -= 1; } },
+    jaegerbrau: { duration: 5, onStart: x => { x.evasion += 0.2; x.hitChance += 0.1; x.critChance += 0.2; }, onEnd: x => { x.evasion -= 0.2; x.hitChance -= 0.1; x.critChance -= 0.2; } },
     protected: { duration: 3 },
 };
 
