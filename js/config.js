c6be0d1c9bbc9dff9aa5853f5de3d2ce54a09a6e
rmtsukuru <samcollegeemail@gmail.com>
2019-03-23 17:10:03 -0700

    Added a debug config flag for later.

--- js/config.js
+++ js/config.js
@@ -2,5 +2,6 @@ const FPS = 60;
 const ASPECT_RATIO = 4 / 3;
 const BASE_HEIGHT = 480;
 const MASTER_VOLUME = 1;
+const DEBUG = true;
 const SHOW_CONDITIONS = true;
 const TRUE_HIT = true;
--- js/scene.js
+++ js/scene.js
@@ -9,7 +9,7 @@ Scene.prototype.update = function() {
 
 Scene.prototype.draw = function() {
     drawRect(0, 0, canvasWidth, canvasHeight, '#000');
-    if (TRUE_HIT) {
+    if (TRUE_HIT && DEBUG) {
         drawText('TRUE HIT', 545, 18, '#36f');
     }
     for (var i = 0; i < party.length; i++) {
@@ -25,7 +25,7 @@ Scene.prototype.draw = function() {
             drawText(data.resourceName + ': ' + data.resource, x, 410);
         }
         drawText('EVA: ' + formatPercent(data.evasion), x, 435);
-        if (SHOW_CONDITIONS) {
+        if (SHOW_CONDITIONS && DEBUG) {
             drawText(data.conditions.map(x => x.name.charAt(0)).join('').toUpperCase(), x, 460);
         }
     }
@@ -34,7 +34,7 @@ Scene.prototype.draw = function() {
             var text = 'Monster ' + (i + 1) + ' HP: ' + Math.max(0, enemy.health) + '/' + enemy.maxHealth;
             drawText(text, 50 + 330 * (i % 2), 50 + 60 * Math.floor(i / 2));
             drawText('ACC: ' + formatPercent(enemy.accuracy), 65 + 330 * (i % 2), 70 + 60 * Math.floor(i / 2));
-            if (SHOW_CONDITIONS) {
+            if (SHOW_CONDITIONS && DEBUG) {
                 drawText(enemy.conditions.map(x => x.name.charAt(0)).join('').toUpperCase(), 165 + 330 * (i % 2), 70 + 60 * Math.floor(i / 2));
             }
         });
